1baea8e785c5878bc4d33dd0468aaf649703f5d4
Abderrezzak-Hamada <9024905@student.zadkine.nl>
2026-03-05 12:00:51 +0100

    Update gameUtils.test.js

--- src/uitleg-les/gameUtils.test.js
+++ src/uitleg-les/gameUtils.test.js
@@ -1,8 +1,16 @@
 import {getRank, addScores, hasWon, getHighScore} from './gameUtils';
 
-test('getRank returns correct rank based on score', () => {
-  expect(getRank(0)).toBe('Beginner');
-  expect(getRank(10)).toBe('Intermediate');
-  expect(getRank(20)).toBe('Advanced');
-  expect(getRank(30)).toBe('Expert');
-});
+Test('Geef Noob terug met score 0', () => {
+    const result = getRank(0);
+    expect(result).toBe('Noob')
+})
+
+Test('Geef Pro terug met score 55', () => {
+    const result = getRank(55);
+    expect(result).toBe('Pro')
+})
+
+test("tel twee scores bij elkaar op", () => {
+    const result = addScores(20, 30);
+    expect(result).toBe(50);
+})
